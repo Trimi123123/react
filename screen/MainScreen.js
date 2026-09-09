28bb3920a0ad90ef71e8d52b3d32d984e9da1d5f
Trimi123123 <trimmerovci10@gmail.com>
2026-09-09 19:16:26 +0200

    354

---
+++ screen/MainScreen.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Text, Stylesheet , View } from "react-native-web"
+
+
+const MainScreen =()=> {
+    return
+    <View>
+    <Text>hello</Text>  
+    <Text>hello</Text> 
+
+    </View>
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+
+
+    }
+})
+export default MainScreen;
