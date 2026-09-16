988511426158897bab7fe374b5f6c15d241257c6
Trimi123123 <trimmerovci10@gmail.com>
2026-09-16 19:11:57 +0200

    4

---
+++ react_app/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Trim</Text>
+      <Text>Merovci</Text>
+      <Text>07/01/2013</Text>
+      <Text>programming</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+   border:30,
+   borderColor:blue
+  }
+});
